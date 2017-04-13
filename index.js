f37dd7320cd26dd7015c2513c956aedf060d64c5
Marc Ferland <mferland@esentire.com>
2017-04-12 23:19:38 -0400

    Added some routes

--- index.js
+++ index.js
@@ -17,18 +17,67 @@ function randomFloatBetween(minValue, maxValue, precision = 2, num = 30) {
 app.use(cors())
 app.use(compression())
 
-app.get('/api/v1/threats/total', (req, res) => {
-  try {
-    return res.status(200).json({
-      success: true,
-      count: randomFloatBetween(100000, 200000)
-    })
-  } catch(err) {
-    return res.status(400).json({
-      err: err
-    }).end()
-  }
-})
+app
+  .get('/api/v1/threats/total', (req, res) => {
+    try {
+      return res.status(200).json({
+        success: true,
+        count: randomFloatBetween(0, 200000)
+      })
+    } catch(err) {
+      return res.status(400).json({
+        err: err
+      }).end()
+    }
+  })
+  .get('/api/v1/disrupted_connections/total', (req, res) => {
+    try {
+      return res.status(200).json({
+        success: true,
+        count: randomFloatBetween(0, 100000)
+      })
+    } catch(err) {
+      return res.status(400).json({
+        err: err
+      }).end()
+    }
+  })
+  .get('/api/v1/auto_notifications/total', (req, res) => {
+    try {
+      return res.status(200).json({
+        success: true,
+        count: randomFloatBetween(0, 20000)
+      })
+    } catch(err) {
+      return res.status(400).json({
+        err: err
+      }).end()
+    }
+  })
+  .get('/api/v1/messages/total', (req, res) => {
+    try {
+      return res.status(200).json({
+        success: true,
+        count: randomFloatBetween(0, 500000)
+      })
+    } catch(err) {
+      return res.status(400).json({
+        err: err
+      }).end()
+    }
+  })
+  .get('/api/v1/targeted_assets/total', (req, res) => {
+    try {
+      return res.status(200).json({
+        success: true,
+        count: randomFloatBetween(0, 15000)
+      })
+    } catch(err) {
+      return res.status(400).json({
+        err: err
+      }).end()
+    }
+  })
 
 app.listen(port, () => {
   console.log(`API is running at http://localhost:${port}/`)
